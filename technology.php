e28a557847439bbe4f141b6b9baf036544d216ee
tleifj <tleifj@Traviss-MacBook-Pro.local>
2015-08-25 16:20:11 -0700

    Update Tech page

--- technology.php
+++ technology.php
@@ -18,7 +18,7 @@
 	?>
 
 <div id="copy">
-	<div class="big">Triton has delivered its first product: PhycoShield&trade; MAA</div>
+	<div class="big">Triton has delivered its first product: Triton MAA</div>
 	<p>Triton has developed a family of proteins that may help in the prevention of intestinal diseases. This family of proteins is biologically identical to those which occur in nature. These proteins help improve digestive tract health, and Triton’s first product is called Mammary Associated Amyloid (MAA), which is a protein found in the colostrum (first milk) of all mammals, and that stimulates the production of a mucus coating in the digestive tract. Mucus coatings help to:</p>
       <ul>
 			<li>Prevent colonization of pathogenic bacteria and reduce viral infection rates</li>
@@ -28,7 +28,7 @@
       <p>MAA is active in mammals of all ages, and is a potentially powerful  and natural way to address intestinal disease in livestock, companion animals, and humans.  It is a major component of colostrum, the safety and efficacy of which has been tested for decades. Over 33 related studies have been conducted on over 2,750 mammalian subjects, including 1100 humans. In these studies, zero negative effects were observed and many of the studies showed positive results from use of colostrum.  As a result, fractionated colostrum is an approved product by the FDA for use in treating certain digestive disorders, such as ulcerative colitis.</p>
       <p style="margin-top:20px; margin-bottom:20px;"><img src="img/technology-maa.jpg" width="570" height="380"></p>
       <p>Algae are an ideal platform for producing and delivering proteins like MAA.  Algae can be a high-quality feed additive on its own, and can act as a safe and effective means of oral delivery of MAA. By delivering MAA with algae, Triton avoids the need for purification, and so when combined with the comparatively low cost of algae production, the end result is that Triton can produce MAA that is affordable to farmers and consumers alike.  MAA has already drawn considerable commercial interest, and Triton has a number of other impactful proteins which it intends to bring to market over the next several years.</p>
-    <p>Learn more about the <a href="technology-platform.php">Triton PhycoLogix&trade; Platform.</a></p>
+    <p>Learn more about the <a href="technology-platform.php">Triton Platform.</a></p>
 
     </div>
   </div>
